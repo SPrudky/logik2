ba259e5e7e19adc88b4364665651af185a06500c
Samuel Prudký <Prudky.Samuel@stud.vassboskovice.cz>
2022-05-31 11:08:13 +0200

    Přidejte soubory projektu.

---
+++ logik2/pin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace logik2
+{
+    public class pin
+    {
+        public Color Color { get; set; }
+    }
+    public static class Pincolor
+    {
+        public static Color Color1 = Colors.Red;
+        public static Color Color2 = Colors.Green;
+        public static Color Color3 = Colors.Yellow;
+        public static Color Color4 = Colors.Blue;
+        public static Color Color5 = Colors.Orange;
+        public static Color Color6 = Colors.Purple;
+
+
+
+    }
+}

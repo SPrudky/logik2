ed4187bbdd0dc6cf7dbd7f6e43fee332e162c10a
Samuel Prudký <Prudky.Samuel@stud.vassboskovice.cz>
2022-06-14 11:29:33 +0200

    skola

---
+++ logik2/game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logik2
+{
+    public class game
+    {
+        public List<Attempt> Attempts { get; set; }
+        public Attempt ActualAttempt = new Attempt();
+        public game()
+        {
+            Attempts = new List<Attempt>();
+            ActualAttempt = new Attempt();
+
+        }
+        public void EvaluateActualAttempt()
+        {
+
+        }
+        
+    }   
+}
+
